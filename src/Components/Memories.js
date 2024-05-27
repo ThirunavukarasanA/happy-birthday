75ceed2c72d1076ee1d9ed39f57511e9a1aae169
ThirunavukarasanA <thirunavukarasan@rvsgroup.com>
2024-05-27 17:06:24 +0530

    content

--- src/Components/Memories.js
+++ src/Components/Memories.js
@@ -6,7 +6,7 @@ export default function Memories() {
     <div>
       <div className="py-20 container mx-auto 2xl:px-10 xl:px-7 lg:px-0 lge:px-7 md:px-4 mdsm:px-4 sm:px-5">
         <p className="2xl:text-[56px] xl:text-[56px] lg:text-[56px] lge:text-[56px] md:text-[50px] mdsm:text-[40px] sm:text-[30px] text-iphone font-semibold sm:px-1 leading-tight pb-16">
-          Relax, it's iPhone
+          Memories Througt Time
         </p>
         <div className="2xl:block xl:block lg:block lge:block md:hidden mdsm:hidden sm:hidden">
           <img src={Relax} alt="Relax it's iPhone" className="rounded-3xl" />
